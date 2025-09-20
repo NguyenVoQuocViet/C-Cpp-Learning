485d94470b469bfdc8fde38e5b1eee8da65c4152
Nguyen Vo Quoc Viet <nvqv123@gmail.com>
2025-09-20 23:35:23 +0700

    Them file maximum_distaces.cpp

---
+++ Lap trinh Cpp/maximum_distaces.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <map>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    map<int, int> mp;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int x = a[i] + k;
+        int y = a[i] - k;
+        if(mp.find(x) != mp.end())
+        {
+            cnt = max(cnt, i - mp[x]);
+        }
+        if(mp.find(y) != mp.end())
+        {
+            cnt = max(cnt, i - mp[y]);
+        }
+        if(mp.find(a[i]) == mp.end())
+        {
+            mp[a[i]] = i;
+        }
+    }
+    cout << (cnt == 0 ? -1 : cnt) << endl;
+    return 0;
+}
