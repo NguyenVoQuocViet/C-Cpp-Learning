ce7f35d9d98de55faedb25eeaff7784a9de5bb6b
NguyenVoQuocViet <nvqv123@gmail.com>
2025-11-12 23:49:56 +0700

    them file so_phep_toan_nho_nhat_de_day_thanh_1.cpp

---
+++ Lap trinh Cpp/so_phep_toan_nho_nhat_de_day_thanh_1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minOperations(vector<int> a)
+{
+    int n = a.size();
+    int ones = count(a.begin(), a.end(), 1);
+    if (ones > 1)
+        return n - ones;
+    int g = 0;
+    int min_len = INT_MAX;
+    for (int i = 0; i < n - 1; i++)
+    {
+        g = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            g = gcd(g, a[j]);
+            if (g == 1)
+            {
+                min_len = min(j - i, min_len);
+                break;
+            }
+        }
+    }
+    if (min_len == INT_MAX)
+        return -1;
+    return min_len + n - 2;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a)
+        cin >> x;
+    cout << minOperations(a);
+    return 0;
+}
